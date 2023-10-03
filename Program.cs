3d589c07762062c29b528eb7b07852167a004117
Radu Elisei <45122923+raduElisei@users.noreply.github.com>
2023-10-03 19:27:52 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Lab2Problema2EliseiRadu;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Scrie deimpartit:");
+        decimal deimpartit = Convert.ToDecimal(Console.ReadLine());
+        Console.WriteLine("Scrie impartitor");
+        decimal impartitor = Convert.ToDecimal(Console.ReadLine());
+        decimal rezultat = deimpartit / impartitor;
+
+        Console.WriteLine("Rezultatul este " + rezultat);
+
+        if((rezultat % 1) == 0)
+        {
+            Console.WriteLine("Impartirea este exacta.");
+        }
+        else
+        {
+            Console.WriteLine("Impartirea nu este exacta.");
+        }
+    }
+}
